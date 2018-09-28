e59196de126bc486bcba98463b50789bb5ae4c79
Xue Chen <xueqingc@gmail.com>
2018-09-28 16:30:37 -0700

    pratice

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,18 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  let counts = {};
+  let maxChar = str[0];
+
+  for (let i = 0; i < str.length; i++){
+    let char = str[i];
+    counts[char] = counts[char] + 1 || 1;
+    if (counts[char] > counts[maxChar]){
+      maxChar = char;
+    }
+  }
+  return maxChar;
+}
 
 module.exports = maxChar;
